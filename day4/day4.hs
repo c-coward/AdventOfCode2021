f11935fc2586f9e97d792bfe04e26d0cfd449380
Josh Coward <jrc0114@auburn.edu>
2021-12-04 17:52:11 -0600

    Cleanup and refactoring

--- day4/day4.hs
+++ day4/day4.hs
@@ -1,4 +1,3 @@
-import Control.Applicative ((<|>))
 import Data.Array.IArray (Array,
     listArray, assocs, (!), (//), elems)
 import Data.Char (isDigit)
@@ -8,17 +7,16 @@ import Data.Maybe (catMaybes, fromJust)
 type Cell = Maybe Int
 type Pos = (Int, Int)
 
-type BoardBland = (Int, Array Pos Cell) -- Board without identifier
+-- Board with identifier and last-command memory
 type Board = (Int, (Int, Array Pos Cell))
 
 main = do
    (input, boards) <- readFile "input.txt" >>= return . parseInp . lines
    putStrLn $ (++) "Part 1: " $ show $ part1 boards input
    putStrLn $ (++) "Part 2: " $ show $ part2 boards input
-   return ()
 
-part1 bs = getScore . winner . findBingos . scanl (updateBoards) bs
-part2 bs = getScore . lastWinner . findBingos . scanl (updateBoards) bs
+part1 bs = getScore . winner . findBingos . scanl updateBoards bs
+part2 bs = getScore . lastWinner . findBingos . scanl updateBoards bs
 
 matchBoard :: Board -> Board -> Bool
 matchBoard (x, _) (y, _) = x == y
@@ -28,7 +26,7 @@ findBingos :: [[Board]] -> [[Board]]
 findBingos = map (filter (or . sequence [checkRows, checkCols]))
 
 -- Find the first winning board
-winner = head . head . dropWhile (null)
+winner = head . head . dropWhile null
 -- Find the last winning board
 lastWinner = last . nubBy matchBoard . concat
 
@@ -43,17 +41,19 @@ getPosLine s = init . scanl tupAdd s . replicate 5
 
 -- See whether a board has a bingo based on position criteria
 checkBingo :: Board -> Pos -> Pos -> Bool
-checkBingo (_, ((_, b))) s = all (null . (b ! )) . getPosLine s
+checkBingo (_, (_, b)) s = all (null . (b ! )) . getPosLine s
 
-checkRows b = or $ map (flip (checkBingo b) (1, 0)) (getPosLine (1, 1) (0, 1))
-checkCols b = or $ map (flip (checkBingo b) (0, 1)) (getPosLine (1, 1) (1, 0))
+checkRows b = any (flip (checkBingo b) (1, 0)) (getPosLine (1, 1) (0, 1))
+checkCols b = any (flip (checkBingo b) (0, 1)) (getPosLine (1, 1) (1, 0))
 
 -- Mark matching cells as Nothing (null)
 markCell :: Cell -> (a, Cell) -> (a, Cell)
 markCell x (i, y) = if y == x then (i, Nothing) else (i, y)
 
+-- Update a board with the next number called
 updateBoard :: Cell -> Board -> Board
-updateBoard c (i, (_, b))= (i, (,) (fromJust c) $ (b //) $ map (markCell c) $ assocs b)
+updateBoard c (i, (_, b))= (i, (,) (fromJust 0 c) $ (b //) $ map (markCell c) $ assocs b)
+updateBoards :: [Board] -> Cell -> [Board]
 updateBoards bs c = map (updateBoard c) bs
 
 tupAdd :: Pos -> Pos -> Pos
@@ -61,13 +61,11 @@ tupAdd (a, b) (c, d) = (a+c, b+d)
 
 parseInp :: [String] -> ([Cell], [Board])
 parseInp (x:xs) = (parseInts x, parseBoards xs) where
-    parseBoard :: [String] -> BoardBland
-    parseBoard = (,) (-1) . listArray ((1,1),(5,5)) . concat . map parseInts
+    parseBoard = (,) (-1) . listArray ((1,1),(5,5)) . concatMap parseInts
 
     -- Associate each board with an ID
-    identify :: [BoardBland] -> [Board]
-    identify bs = zip ([1..length bs]) bs
- 
+    identify bs = zip [1..length bs] bs
+
     groupInp :: ([a] -> Bool) -> (a -> Bool) -> [a] -> [[a]]
     groupInp g f = filter g . groupBy (\a b -> all f [a,b])
 
