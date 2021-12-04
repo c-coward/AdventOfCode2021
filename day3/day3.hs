d46ee5a394c52716215884ac91295e7287ecc756
Josh Coward <jrc0114@auburn.edu>
2021-12-03 23:03:48 -0600

    Cleaned up day3 part 2 a little bit

--- day3/day3.hs
+++ day3/day3.hs
@@ -3,32 +3,37 @@ import Data.Char (digitToInt)
 
 import Debug.Trace (traceShow)
 
+type BitString = [Int] -- List of 0s and 1s
+
 main = do
     input <- readFile "input.txt" -- Do the common input processing
-        >>= return . map (map (negate0 . digitToInt)) . lines
+        >>= return . map (map (digitToInt)) . lines
 
     putStrLn $ (++) "Part 1: " $ show $ part1 input
     putStrLn $ (++) "Part 2: " $ show $ part2 input
 
-part1 = complProd [id, map (1-)] . map (reverseN0 . sum) . transpose
-part2 = complProd [filterReduce id, filterReduce negate]
+part1 = seqProd [id, map (1-)] . map (reverseN0 . sum . map negate0) . transpose
+part2 = seqProd [filterReduce id, filterReduce (1-)]
 
-filterReduce :: (Int -> Int) -> [[Int]] -> [Int]
+filterReduce :: (Int -> Int) -> [BitString] -> BitString
 filterReduce f xs = case length xs == 1 of
-    True -> map reverseN0 $ head xs
+    True -> head xs
     _    -> let
-        fBit = f $ negate0 $ (1-) $ reverseN0 $ sum $ map head xs
+        fBit = f $ reverseN0 $ sum $ map (negate0 . head) xs
         xs' = map tail $ filter ((== fBit) . head) xs
-        in (reverseN0 fBit : filterReduce f xs')
+        in (fBit : filterReduce f xs')
 
-bToI :: [Int] -> Int
+-- Convert string a bits to decimal integer
+bToI :: BitString -> Int
 bToI = foldl (\b a -> 2*b + a) 0
 
--- By converting 0s to -1s, summing over the list gives the "count" of each bit
 negate0, reverseN0 :: Int -> Int
+-- By converting 0s to -1s, summing over the list gives the "count" of each bit
 negate0 = (subtract 1) . (2 *)
 -- reverseN0 provides a convenient way to get back to the binary representation
+-- however, 0s are converted to 1s due to the demands of the problem
 reverseN0 = fromEnum . (>= 0)
 
-complProd :: [a -> [Int]] -> a -> Int
-complProd fs = product . map bToI . sequence fs
+-- Product over a sequencing of bitstring functions
+seqProd :: [a -> BitString] -> a -> Int
+seqProd fs = product . map bToI . sequence fs
