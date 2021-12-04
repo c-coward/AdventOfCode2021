6e2b831562153ba791b7bb24aa324cd26e4ec110
Josh Coward <jrc0114@auburn.edu>
2021-12-04 03:01:27 -0600

    Completed day 4

---
+++ day4/day4.hs
@@ -0,0 +1,76 @@
+import Control.Applicative ((<|>))
+import Data.Array.IArray (Array,
+    listArray, assocs, (!), (//), elems)
+import Data.Char (isDigit)
+import Data.List (groupBy, nubBy)
+import Data.Maybe (catMaybes, fromJust)
+
+type Cell = Maybe Int
+type Pos = (Int, Int)
+
+type BoardBland = (Int, Array Pos Cell) -- Board without identifier
+type Board = (Int, (Int, Array Pos Cell))
+
+main = do
+   (input, boards) <- readFile "input.txt" >>= return . parseInp . lines
+   putStrLn $ (++) "Part 1: " $ show $ part1 boards input
+   putStrLn $ (++) "Part 2: " $ show $ part2 boards input
+   return ()
+
+part1 bs = getScore . winner . findBingos . scanl (updateBoards) bs
+part2 bs = getScore . lastWinner . findBingos . scanl (updateBoards) bs
+
+matchBoard :: Board -> Board -> Bool
+matchBoard (x, _) (y, _) = x == y
+
+-- Map over the generations of boards, filtering for winning boards in each one
+findBingos :: [[Board]] -> [[Board]]
+findBingos = map (filter (or . sequence [checkRows, checkCols]))
+
+-- Find the first winning board
+winner = head . head . dropWhile (null)
+-- Find the last winning board
+lastWinner = last . nubBy matchBoard . concat
+
+getScore :: Board -> Int
+getScore (_, (i, b)) = (i*) $ sum $ catMaybes $ elems b
+
+-- Get all points on the line defined by the two points
+-- First point describes the starting position
+-- Second point describes the slope
+getPosLine :: Pos -> Pos -> [Pos]
+getPosLine s = init . scanl tupAdd s . replicate 5
+
+-- See whether a board has a bingo based on position criteria
+checkBingo :: Board -> Pos -> Pos -> Bool
+checkBingo (_, ((_, b))) s = all (null . (b ! )) . getPosLine s
+
+checkRows b = or $ map (flip (checkBingo b) (1, 0)) (getPosLine (1, 1) (0, 1))
+checkCols b = or $ map (flip (checkBingo b) (0, 1)) (getPosLine (1, 1) (1, 0))
+
+-- Mark matching cells as Nothing (null)
+markCell :: Cell -> (a, Cell) -> (a, Cell)
+markCell x (i, y) = if y == x then (i, Nothing) else (i, y)
+
+updateBoard :: Cell -> Board -> Board
+updateBoard c (i, (_, b))= (i, (,) (fromJust c) $ (b //) $ map (markCell c) $ assocs b)
+updateBoards bs c = map (updateBoard c) bs
+
+tupAdd :: Pos -> Pos -> Pos
+tupAdd (a, b) (c, d) = (a+c, b+d)
+
+parseInp :: [String] -> ([Cell], [Board])
+parseInp (x:xs) = (parseInts x, parseBoards xs) where
+    parseBoard :: [String] -> BoardBland
+    parseBoard = (,) (-1) . listArray ((1,1),(5,5)) . concat . map parseInts
+
+    -- Associate each board with an ID
+    identify :: [BoardBland] -> [Board]
+    identify bs = zip ([1..length bs]) bs
+ 
+    groupInp :: ([a] -> Bool) -> (a -> Bool) -> [a] -> [[a]]
+    groupInp g f = filter g . groupBy (\a b -> all f [a,b])
+
+    -- Returns Maybe Int's since all the numbers I needed were Maybes anyways
+    parseInts = map (Just . read) . groupInp (isDigit . head) isDigit :: String -> [Cell]
+    parseBoards = identify . map parseBoard . groupInp (/= [""]) (not . null)
