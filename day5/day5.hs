80135e8961c41db246521d329af6317b0a18ffbd
Josh Coward <jrc0114@auburn.edu>
2021-12-05 01:20:26 -0600

    Removed useless function

--- day5/day5.hs
+++ day5/day5.hs
@@ -25,7 +25,6 @@ getLineSegPoints (a, b) = go a b where
         | otherwise = l : go (l .+. m) r
 
     (x0, y0) .+. (x1, y1) = (x0 + x1, y0 + y1)
-    (x0, y0) ? (x1, y1) = (x0 > x1) || (y0 > y1)
 
 slope :: LineSeg -> Point
 slope ((x0, y0), (x1, y1)) = reduce (x1 - x0, y1 - y0)
