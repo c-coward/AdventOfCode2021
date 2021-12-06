b7557bf9e5235fd1190396cca2e1561485056406
Josh Coward <jrc0114@auburn.edu>
2021-12-06 15:15:28 -0600

    Refactored day 4, inspo: https://www.youtube.com/watch?v=mCCdp9cupK4

--- day4/day4.hs
+++ day4/day4.hs
@@ -1,74 +1,34 @@
-import Data.Array.IArray (Array,
-    listArray, assocs, (!), (//), elems)
 import Data.Char (isDigit)
-import Data.List (groupBy, nubBy)
-import Data.Maybe (catMaybes, fromJust)
+import Data.List (groupBy, transpose, inits, sort, (\\))
+import Data.Maybe (catMaybes)
 
-type Cell = Maybe Int
-type Pos = (Int, Int)
-
--- Board with identifier and last-command memory
-type Board = (Int, (Int, Array Pos Cell))
+type Board = [[Int]]
 
 main = do
-   (input, boards) <- readFile "input.txt" >>= return . parseInp . lines
-   putStrLn $ (++) "Part 1: " $ show $ part1 boards input
-   putStrLn $ (++) "Part 2: " $ show $ part2 boards input
+   (calls, boards) <- readFile "input.txt" >>= return . parseInp . lines
+   putStrLn $ (++) "Part 1: " $ show $ part1 calls boards
+   putStrLn $ (++) "Part 2: " $ show $ part2 calls boards
 
-part1 bs = getScore . winner . findBingos . scanl updateBoards bs
-part2 bs = getScore . lastWinner . findBingos . scanl updateBoards bs
+part1 cs = snd . head . sort . map (flip winScore cs)
+part2 cs = snd . last . sort . map (flip winScore cs)
 
-matchBoard :: Board -> Board -> Bool
-matchBoard (x, _) (y, _) = x == y
+winScore :: Board -> [[Int]] -> (Int, Int)
+winScore b = head . catMaybes . map (score b)
 
--- Map over the generations of boards, filtering for winning boards in each one
-findBingos :: [[Board]] -> [[Board]]
-findBingos = map (filter (or . sequence [checkRows, checkCols]))
+score :: Board -> [Int] -> Maybe (Int, Int)
+score b c
+    | isBingo c b = Just (length c, (*) (last c) $ sum $ concat b \\ c)
+    | otherwise   = Nothing
 
--- Find the first winning board
-winner = head . head . dropWhile null
--- Find the last winning board
-lastWinner = last . nubBy matchBoard . concat
+checkRows, checkCols, isBingo :: [Int] -> Board -> Bool
+checkRows cs = any (and . \xs -> map (`elem` cs) xs)
+checkCols cs = checkRows cs . transpose
+isBingo cs = or . sequence [checkRows cs, checkCols cs]
 
-getScore :: Board -> Int
-getScore (_, (i, b)) = (i*) $ sum $ catMaybes $ elems b
+parseInp :: [String] -> ([[Int]], [Board])
+parseInp (x:xs) = (inits (parseInts x), parseBoards xs)
 
--- Get all points on the line defined by the two points
--- First point describes the starting position
--- Second point describes the slope
-getPosLine :: Pos -> Pos -> [Pos]
-getPosLine s = init . scanl tupAdd s . replicate 5
-
--- See whether a board has a bingo based on position criteria
-checkBingo :: Board -> Pos -> Pos -> Bool
-checkBingo (_, (_, b)) s = all (null . (b ! )) . getPosLine s
-
-checkRows b = any (flip (checkBingo b) (1, 0)) (getPosLine (1, 1) (0, 1))
-checkCols b = any (flip (checkBingo b) (0, 1)) (getPosLine (1, 1) (1, 0))
-
--- Mark matching cells as Nothing (null)
-markCell :: Cell -> (a, Cell) -> (a, Cell)
-markCell x (i, y) = if y == x then (i, Nothing) else (i, y)
-
--- Update a board with the next number called
-updateBoard :: Cell -> Board -> Board
-updateBoard c (i, (_, b))= (i, (,) (fromJust c) $ (b //) $ map (markCell c) $ assocs b)
-updateBoards :: [Board] -> Cell -> [Board]
-updateBoards bs c = map (updateBoard c) bs
-
-tupAdd :: Pos -> Pos -> Pos
-tupAdd (a, b) (c, d) = (a+c, b+d)
-
-parseInp :: [String] -> ([Cell], [Board])
-parseInp (x:xs) = (parseInts x, parseBoards xs) where
-    parseBoard = (,) (-1) . listArray ((1,1),(5,5)) . concatMap parseInts
-
-    -- Associate each board with an ID
-    identify bs = zip [1..length bs] bs
-
-    groupInp :: ([a] -> Bool) -> (a -> Bool) -> [a] -> [[a]]
-    groupInp g f = filter g . groupBy (\a b -> f a && f b)
-
-    -- Returns Maybe Int's since all the numbers I needed were Maybes anyways
-    parseInts = map (Just . read) . groupInp (isDigit . head) isDigit :: String -> [Cell]
-    parseBoards = identify . map parseBoard . groupInp (/= [""]) (not . null)
+groupInp :: ([a] -> Bool) -> (a -> Bool) -> [a] -> [[a]]
+groupInp g f = filter g . groupBy (\a b -> f a && f b)
+parseInts = map read . groupInp (isDigit . head) isDigit :: String -> [Int]
+parseBoards = map (map parseInts) . groupInp (/= [""]) (not . null)
