d0570fe1d4dbcb2379b093af1dfe1d0b2802538a
Josh Coward <jrc0114@auburn.edu>
2021-12-08 23:21:36 -0600

    progress on part 2

--- day8/day8.hs
+++ day8/day8.hs
@@ -1,11 +1,15 @@
-import Data.List (intersect)
-import Data.Map (Map, findWithDefault)
+import Data.Set (Set, union, intersection, union, fromList)
+import Data.Map (Map, findWithDefault, empty, insert, insertWith)
 
 main = do
-    input <- readFile "input.txt" >>= return . lines
+    input <- readFile "input.txt" >>= return . map splitInp . lines
     putStrLn $ show $ part1 input
+    putStrLn $ show $ part2 input
 
-part1 = sum . map (length . findCounts [2,3,4,7] . snd . splitInp)
+part1 = sum . map (length . findCounts [2,3,4,7] . snd)
+part2 = solve . fst . head
+
+solve xs = find069 xs $ find235 xs $ find7 xs $ find4 xs $ find1 xs empty
 
 findCounts :: [Int] -> [String] -> [String]
 findCounts cs = filter ((`elem` cs) . length)
@@ -13,11 +17,30 @@ findCounts cs = filter ((`elem` cs) . length)
 splitInp :: String -> ([String], [String])
 splitInp = (\(a, b) -> (a, tail b)) . span (/= "|") . words
 
-zero  = [0,1,2,4,5,6];  one   = [2,5]
-two   = [0,2,3,4,6];    three = [0,2,3,5,6]
-four  = [1,2,3,5];      five  = [0,1,3,5,6]
-six   = [0,1,3,4,5,6];  seven = [0,2,5]
-eight = [0..6];         nine  = [0,1,2,3,5,6]
+posMap :: Map Char (Set Int)
+posMap = foldl (\m x -> insert x eight m) empty "abcdefg"
+
+-- Identify easy cases
+findEasy :: Int -> Set Int -> [String] -> Map Char (Set Int) -> Map Char (Set Int)
+findEasy k s xss m = foldl (\m' xs ->
+        foldl (\m'' x -> insertWith (intersection) x s m'') m' xs)
+    m (filter ((== k) . length) xss)
+
+find1 = findEasy 2 one
+find7 = findEasy 3 seven
+find4 = findEasy 4 four
+find8 = findEasy 7 eight -- Useless, since everything is assumed an 8 at the start
+
+-- Hard cases can be defined by unions
+find235 = findEasy 5 (two `union` three `union` five)
+find069 = findEasy 6 (zero `union` six `union` nine)
+
+-- Signal Position Set definitions
+zero  = fromList [0,1,2,4,5,6];  one   = fromList [2,5]
+two   = fromList [0,2,3,4,6];    three = fromList [0,2,3,5,6]
+four  = fromList [1,2,3,5];      five  = fromList [0,1,3,5,6]
+six   = fromList [0,1,3,4,5,6];  seven = fromList [0,2,5]
+eight = fromList [0..6];         nine  = fromList [0,1,2,3,5,6]
 
 {-
 SIGNAL POSITIONS:
