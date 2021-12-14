1e8a6097ee0ed8208d960a36e078591b1a0007f8
Josh Coward <jrc0114@auburn.edu>
2021-12-14 17:25:31 -0600

    Refactored day14

--- day14/day14.hs
+++ day14/day14.hs
@@ -10,28 +10,26 @@ main = do
     putStrLn $ (++) "Part 1: " $ show $ part1 rls tmp
     putStrLn $ (++) "Part 2: " $ show $ part2 rls tmp
 
-part1 rls tmp = uncurry (-) $ lastHead $ sort $ elems $ countOcc tmp $ (!! 10) $ iterate (applyRules rls) $ mkPairs tmp
-part2 rls tmp = uncurry (-) $ lastHead $ sort $ elems $ countOcc tmp $ (!! 40) $ iterate (applyRules rls) $ mkPairs tmp
+part1 rls = uncurry (-) . lastHead . sort . elems . countOcc . (!! 10) . iterate (applyRules rls)
+part2 rls = uncurry (-) . lastHead . sort . elems . countOcc . (!! 40) . iterate (applyRules rls)
 
 lastHead = (\xs -> (last xs, head xs))
 
--- Only count the first char in a pair to avoid double counting on overlaps
--- Needs the original string since the last char won't get counted normally
-countOcc :: String -> Pairs -> Map Char Int
-countOcc s = insertWith (+) (last s) 1 . fromListWith (+) . map (\((a, b), c) -> (a, c)) . assocs
+countOcc :: Pairs -> Map Char Int
+countOcc = fromListWith (+) . map (\((a, b), c) -> (a, c)) . assocs
 
 applyRule :: Rules -> ((Char, Char), Int) -> [((Char, Char), Int)]
+applyRule _ ((b, '0'), c)  = [((b, '0'), c)] -- Special case for the last character
 applyRule rls (p@(a,b), c) = [((a, rls ! p), c), ((rls ! p, b), c)]
 
 applyRules :: Rules -> Pairs -> Pairs
 applyRules rls = fromListWith (+) . concatMap (applyRule rls) . toList
 
-mkPairs :: String -> Pairs
-mkPairs [x] = empty
-mkPairs (a:b:cs) = insertWith (+) (a, b) 1 $ mkPairs (b:cs)
-
-parseInp :: [String] -> (String, Rules)
-parseInp (x:_:xs) = (x, fromList $ map (go . words) xs) where
-
+parseInp :: [String] -> (Pairs, Rules)
+parseInp (x:_:xs) = (mkPairs x, fromList $ map (go . words) xs) where
     go :: [String] -> ((Char, Char), Char)
     go ([a,b]:_:[[c]]) = ((a,b), c)
+
+    mkPairs :: String -> Pairs
+    mkPairs [x] = fromList [((x, '0'), 1)] -- Special case to count the last character
+    mkPairs (a:b:cs) = insertWith (+) (a, b) 1 $ mkPairs (b:cs)
