451d9350f9d6f0293689fbfc9738023028547439
Josh Coward <jrc0114@auburn.edu>
2021-12-20 21:12:17 -0600

    Rewrote using arrays

--- day20/day20.hs
+++ day20/day20.hs
@@ -1,11 +1,11 @@
-import Data.Map (Map, findWithDefault, empty, insert, fromList, elems, keys)
+-- import Data.Map (Map, findWithDefault, empty, insert, fromList, elems, keys)
 import qualified Data.Set as S (unions, fromList, toList)
 import Data.List (groupBy, nub, sort)
-import Data.Array.IArray (Array, (!), listArray, inRange)
+import Data.Array.IArray (Array, (!), array, listArray, inRange, bounds, indices, elems)
 import Data.Function (on)
 
 type Point = (Int, Int)
-type Image = (Map Point Bool, Bool)
+type Image = (Array Point Bool, Bool)
 type Code = Array Int Bool
 
 main = do
@@ -25,28 +25,28 @@ getPtsSize (a, _) = sum $ map fromEnum $ elems a
 
 nextGen :: Code -> Image -> Image
 nextGen code img@(pts, cnd) = let
-    pts' = S.toList $ S.unions $ map (S.fromList . neighbs) $ keys pts
-    in (fromList $ map (decode code img) pts',
+    pts' = S.toList $ S.unions $ map (S.fromList . neighbs) $ indices pts
+    ((ly, lx), (uy, ux)) = bounds pts
+    in (array ((ly-1,lx-1),(uy+1,ux+1)) $ map (decode code img) pts',
         if code ! 0 then not cnd else cnd)
 
 decode :: Code -> Image -> Point -> (Point, Bool)
 decode code img pt = let
     pts' = neighbs pt
-    bits = toIdx $ map (fromEnum . (img ?)) pts'
+    bits = foldl (\b a -> 2*b + a) 0 $ map (fromEnum . (img ?)) pts'
     in (pt, code ! bits)
-
-toIdx :: [Int] -> Int
-toIdx = foldl (\b a -> 2 * b + a) 0
 
 neighbs :: Point -> [Point]
 neighbs (y, x) = [(y + dy, x + dx) | dy <- [-1..1], dx <- [-1..1]]
 
-(m, c) ? p = findWithDefault c p m
+(?) :: Image -> Point -> Bool
+(m, c) ? p
+    | inRange b p = m ! p
+    | otherwise   = c
+    where b = bounds m
 
 parseInp :: [String] -> (Code, Image)
-parseInp (x:_:ys) = (listArray (0, length x - 1) x', (go 0 0 ys, False)) where
+parseInp (x:_:ys) = (listArray (0, length x - 1) x', (go ys', False)) where
     x' = map (== '#') x
-    go _ _ [] = empty
-    go i j ([]:yss) = go (i + 1) 0 yss
-    go i j (('#':ys):yss) = insert (i, j) True $ go i (j + 1) (ys:yss)
-    go i j ((_:ys):yss) = insert (i, j) False $ go i (j + 1) (ys:yss)
+    ys' = map (map (== '#')) ys
+    go zs = listArray ((1, 1), (length zs, length $ head zs)) $ concat zs
