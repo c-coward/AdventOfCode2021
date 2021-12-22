3929c3ffb2841f54e400bba3dde66c96021e2538
Josh Coward <jrc0114@auburn.edu>
2021-12-22 15:56:51 -0600

    Lots of cleanup

--- day22/day22.hs
+++ day22/day22.hs
@@ -1,7 +1,6 @@
 import Data.List (groupBy)
 import Data.Char (isDigit)
 import Data.Function (on)
-import Data.Maybe (catMaybes, fromJust)
 
 -- My main issue when trying to solve this was figuring out how to get all the possible ways two cubes could intersect
 -- This person's method (subdividing all the ranges and combining afterwards) is very clever and simple to implement
@@ -12,57 +11,46 @@ type Range = (Point, Point, Point)
 type Instr = (Bool, Range)
 
 main = do
-    input <- readFile "input.txt" >>= return . map parsePts . lines
+    input <- readFile "input.txt" >>= return . map parseInstr . lines
     putStrLn $ (++) "Part 1: " $ show $ part1 input
     putStrLn $ (++) "Part 2: " $ show $ part2 input
 
-part1 = sum . map volR . catMaybes . map (clampR . snd) . foldl runInstr []
-part2 = sum . map volR . map snd . foldl runInstr []
+part1 = sum . map (volR . clampR) . foldl runInstr []
+part2 = sum . map volR . foldl runInstr []
 
 runInstr :: [Instr] -> Instr -> [Instr]
-runInstr is i@(c, rs) = if c then (i : is') else is' where
-    is' = concatMap (subdivide i) is
+runInstr is i@(c, _) = if c then (i : is') else is'
+    where is' = concatMap (subdivide i) is
 
+vol (l, u) | l > u = 0
 vol (l, u) = u - l + 1
-volR (x, y, z) = product . map vol $ [x, y, z]
+volR (_, (x, y, z)) = product . map vol $ [x, y, z]
 
-clamp (l, u)
-    | l' > u' = Nothing
-    | otherwise = Just (l', u')
-    where (l', u') = (max (-50) l, min 50 u)
-clampR (x, y, z)
-    | any null [x', y', z'] = Nothing
-    | otherwise = Just (fromJust x', fromJust y', fromJust z')
-    where (x', y', z') = (clamp x, clamp y, clamp z)
+clamp (l, u) = (max (-50) l, min 50 u)
+clampR (c, (x, y, z)) = (c, (clamp x, clamp y, clamp z))
 
-overlap :: Point -> Point -> Bool
-overlap (la, ua) (lb, ub) = la <= ub && lb <= ua
+overlap ((la, ua), (lb, ub)) = la <= ub && lb <= ua
 
--- Split a range into subintervals based on overlapping intervals
--- This is at most 3 intervals
+-- Split a range into subintervals based on overlapping intervals, at most 3
 fixOverlap :: Point -> Point -> [Point]
 fixOverlap (la, ua) (lb, ub)
     | la < lb = (la, lb - 1) : fixOverlap (lb, ua) (lb, ub)
     | ua > ub = (ub + 1, ua) : fixOverlap (la, ub) (lb, ub)
     | otherwise = [(la, ua)]
 
--- Split b into all non-overlapping sub intervals with a
 subdivide :: Instr -> Instr -> [Instr]
-subdivide (ac, (ax, ay, az)) (bc, (bx, by, bz))
-    | overlap bx ax && overlap by ay && overlap bz az = 
+subdivide (_, (ax, ay, az)) (bc, (bx, by, bz))
+    | all overlap [(ax, bx), (ay, by), (az, bz)] = 
         [(bc, (x, y, z)) | x <- fixOverlap bx ax,
                            y <- fixOverlap by ay,
                            z <- fixOverlap bz az,
                            -- Filter for subcubes that don't overlap with cube a
-                           (not . all (uncurry overlap)) [(x, ax), (y, ay), (z, az)]]
-    | otherwise = [(bc, (bx, by, bz))]
+                           (not . all overlap) [(x, ax), (y, ay), (z, az)]]
+    | otherwise = [(bc, (bx, by, bz))] -- B doesn't overlap with a, so simply return it
 
-parsePts :: String -> Instr
-parsePts xs = let
+parseInstr :: String -> Instr
+parseInstr xs = (a == "on", pts) where
     a = head . words $ xs
     pts = (\[x,x',y,y',z,z'] -> ((x,x'), (z,z'), (y,y')) )
         . map read . filter (all isIntChar) . groupBy ((==) `on` isIntChar) $ xs
-    in case a of
-        "on" -> (True, pts)
-        "off" -> (False, pts)
 isIntChar = or . sequence [isDigit, (== '-')]
